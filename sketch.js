f4a15761d44f3f3991bf60ca2725bc978c810a9b
Nathanlobo10 <nathanlobo008@gmail.com>
2021-01-01 12:35:30 +0530

    extra pig

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var pig2;
 var backgroundImg,platform;
 
 function preload() {
@@ -27,6 +28,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
+    pig2 = new Pig(810,150);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -58,6 +60,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    pig2.display();
 
     bird.display();
     platform.display();
